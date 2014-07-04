3646743cbdeae45b6045b0bc5fdaebc8422b7677
wuchiasheng <wu.cecc@gmail.com>
2014-07-04 16:28:36 +0800

    Googledatastore dateTime is included

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
     Continuously read the serial port and process IO data received from a remote XBee.
-    Google datastore integration
+    Depedency:1)https://pypi.python.org/pypi/XBee/2.1.0
+    2)database.py
     """
 
 #from xbee import XBee
@@ -9,8 +10,9 @@ from databaseRemote import DBRemoteConnector
 import serial
 import re
 import time
+from datetime import datetime
 import googledatastore as datastore
-
+from googledatastore.helper import *
 #Initialization
 ser = serial.Serial('/dev/ttyS0', 38400)
 #xbee = XBee(ser)
@@ -49,61 +51,32 @@ while True:
             """
         Results=re.findall(r'#(\w+):(\d+[.]?[\d]?[\d]?[\d]?)',msgStr)
 		for result in Results:
-            """
-            Local & Remote MySQL insertion
-            """
-            print result[0]	#quantity
+			print result[0]	#quantity
 			print result[1]	#value
 			d.store(nid,80,result[0],result[1],result)
 			dr.store(nid,80,result[0],result[1],result)
 			
-            """
-            Google datastore insertion
-            """
-            # Create a RPC request to begin a new transaction.
-            req = datastore.BeginTransactionRequest()
-   		 	# Execute the RPC synchronously.
-            resp = datastore.begin_transaction(req)
-            # Get the transaction handle from the response.
-            tx = resp.transaction
-            # Create a RPC request to get entities by key.
-            req = datastore.LookupRequest()
-            # Create a new entity key.
-            key = datastore.Key()
-            # Set the entity key with only one `path_element`: no parent.
-            path = key.path_element.add()
+            
+			#Google datastore integration
+			req = datastore.CommitRequest()
+			req.mode = datastore.CommitRequest.NON_TRANSACTIONAL
+			sensor = req.mutation.insert_auto_id.add()
+            #define entity path
+			path = sensor.key.path_element.add()
             path.kind = 'sensorParser'
-			path.name = time.strftime("%d%m%Y%H%M%S")
-            # Add one key to the lookup request.
-            req.key.extend([key])
-            # Set the transaction, so we get a consistent snapshot of the
-            # entity at the time the transaction started.
-            req.read_options.transaction = tx
-            # Execute the RPC and get the response.
-            resp = datastore.lookup(req)
-            # Create a RPC request to commit the transaction.
-   			req = datastore.CommitRequest()
-   			# Set the transaction to commit.
-            req.transaction = tx
-			entity = req.mutation.insert.add()
-            # Copy the entity key.
-            entity.key.CopyFrom(key)
-            # Add two entity properties:
-            # - a utf-8 string: `question`
-            prop = entity.property.add()
+            # Add 3 entity properties:sensorName,sensorID,dateTime
+            prop = sensor.property.add()
             prop.name = 'sensorID'
             prop.value.string_value = nid
-            # - a 64bit integer: `answer`
-            prop = entity.property.add()
+            prop = sensor.property.add()
             prop.name = 'sensorName'
             prop.value.string_value = result[0]
-			prop = entity.property.add()
+			prop = sensor.property.add()
 			prop.name = 'sensorValue'
 			prop.value.string_value = result[1]
-            # Execute the Commit RPC synchronously and ignore the response:
-            # Apply the insert mutation if the entity was not found and close
-            # the transaction.
-            datastore.commit(req)
-
+			prop = sensor.property.add()
+			prop.name ='dateTime'
+			prop.value.timestamp_microseconds_value =to_timestamp_usec(datetime.now())
+			datastore.commit(req)
 #Termination
 ser.close()
